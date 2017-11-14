e7c6c15a42b033ac8fc1009c043b078edd9e1141
Mikhail Tchernychev <mikhailt@candicontrols.com>
2017-11-13 16:02:24 -0800

    Adding Who_Is_Router to candi shared library

--- bacnet.mk
+++ bacnet.mk
@@ -24,7 +24,9 @@ libfiles += $(bacnet)/demo/handler/h_whois.c $(bacnet)/demo/handler/h_iam.c \
             $(bacnet)/demo/handler/h_rp.c    $(bacnet)/demo/handler/s_iam.c \
             $(bacnet)/demo/handler/s_rp.c    $(bacnet)/demo/handler/s_wp.c  \
 	    $(bacnet)/demo/handler/h_npdu.c  $(bacnet)/demo/handler/s_rpm.c  \
-            $(bacnet)/demo/handler/dlenv.c   $(bacnet)/demo/handler/s_whois.c 
+            $(bacnet)/demo/handler/dlenv.c   $(bacnet)/demo/handler/s_whois.c \
+            $(bacnet)/demo/handler/s_router.c
+
 
 libfiles += $(bacnet)/ports/linux/bip-init.c 
 
