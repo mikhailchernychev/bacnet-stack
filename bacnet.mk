ee2852c7b7d4469de759610363a3531f39cf5b7b
Mikhail Tchernychev <mikhailt@candicontrols.com>
2017-11-22 08:09:01 -0800

    adding COV handler to libbacnet.so

--- bacnet.mk
+++ bacnet.mk
@@ -19,13 +19,13 @@ libfiles := $(filter-out $(bacnet)/src/ucix.c, $(libfiles))
 
 libfiles +=  $(bacnet)/demo/object/device-client.c
 
-libfiles += $(bacnet)/demo/handler/h_whois.c $(bacnet)/demo/handler/h_iam.c \
-            $(bacnet)/demo/handler/noserv.c  $(bacnet)/demo/handler/txbuf.c \
-            $(bacnet)/demo/handler/h_rp.c    $(bacnet)/demo/handler/s_iam.c \
-            $(bacnet)/demo/handler/s_rp.c    $(bacnet)/demo/handler/s_wp.c  \
-	    $(bacnet)/demo/handler/h_npdu.c  $(bacnet)/demo/handler/s_rpm.c  \
-            $(bacnet)/demo/handler/dlenv.c   $(bacnet)/demo/handler/s_whois.c \
-            $(bacnet)/demo/handler/s_router.c
+libfiles += $(bacnet)/demo/handler/h_whois.c   $(bacnet)/demo/handler/h_iam.c \
+            $(bacnet)/demo/handler/noserv.c    $(bacnet)/demo/handler/txbuf.c \
+            $(bacnet)/demo/handler/h_rp.c      $(bacnet)/demo/handler/s_iam.c \
+            $(bacnet)/demo/handler/s_rp.c      $(bacnet)/demo/handler/s_wp.c  \
+	    $(bacnet)/demo/handler/h_npdu.c    $(bacnet)/demo/handler/s_rpm.c  \
+            $(bacnet)/demo/handler/dlenv.c     $(bacnet)/demo/handler/s_whois.c \
+            $(bacnet)/demo/handler/s_router.c  $(bacnet)/demo/handler/s_cov.c \
 
 
 libfiles += $(bacnet)/ports/linux/bip-init.c 
